025a1878074497973a1a38c6beb22a65f93178d7
RabiyaNoorAhmed <rabiyanoorahmed@gmail.com>
2024-05-23 22:10:10 +0500

    Startup Structure

---
+++ src/components/album/DisplayAlbum.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const DisplayAlbum = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default DisplayAlbum
---
+++ src/components/display/AlbumItem.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AlbumItem = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default AlbumItem
