5ebf138811c78703d91c6e6d8e24cac6286bbffd
Syeda Nousheen <nousheens667@gmail.com>
2024-05-24 10:51:55 +0500

    updated app.js file

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Sidebar from './components/sidebar/Sidebar'
 import Player from './components/musicplayer/Player'
+import Display from './components/display/Display'
 
 const App = () => {
   return (
     <div className='h-screen bg-black'>
       <div className='h-[90%] flex'>
         <Sidebar />
+        <Display />
       </div>
       <Player />
     </div>
